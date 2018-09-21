c44075776e3d3feb91f539b6085b9c21359a593a
Jon Koldo Arteaga <401958+jkarteaga@users.noreply.github.com>
2018-09-21 13:46:26 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 export default ({ globalSettings, socialSettings, navLinks }) => (
   <footer className='Footer'>
     <div className='container taCenter'>
-<span class=”copy-left”>© Jon Koldo Arteaga</span>
+<span class="copy-left">© Jon Koldo Arteaga</span>
 
  </div>
   </footer>
