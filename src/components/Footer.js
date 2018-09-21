715be3429a5ea2bffdb0d71582ffb51f64fb0b65
Jon Koldo Arteaga <401958+jkarteaga@users.noreply.github.com>
2018-09-21 13:51:40 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,6 @@ import './Footer.css'
 export default ({ globalSettings, socialSettings, navLinks }) => (
   <footer className='Footer'>
     <div className='container taCenter'>
-<span>© 2017 All rights reserved.</span>   </div>
+Copyleft  </div>
   </footer>
 )
